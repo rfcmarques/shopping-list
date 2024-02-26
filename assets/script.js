026b51eb1acbb9bff7724481bc129f60d6993820
Rui Marques <a.marques1967@gmail.com>
2024-02-26 18:13:12 +0000

    Preventing duplicate entries

--- assets/script.js
+++ assets/script.js
@@ -32,6 +32,11 @@ function onAddItemSubmit(e) {
         itemToEdit.classList.remove('edit-mode');
         itemToEdit.remove();
         isEditMode = false;
+    } else {
+        if (checkIfItemExists(newItem)) {
+            alert('That Item already exists');
+            return;
+        }
     }
 
     addItemToDOM(newItem);
@@ -98,6 +103,12 @@ function onClickItem(e) {
     } else {
         setItemToEdit(e.target);
     }
+}
+
+function checkIfItemExists(item) {
+    const itemsFromStorage = getItemsFromStorage();
+
+    return itemsFromStorage.includes(item);
 }
 
 function setItemToEdit(item) {
